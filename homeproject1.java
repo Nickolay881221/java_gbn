787b9a881ba0311ba0eec78386e5f281d420a4f7
Nick Volodin <Volod-nick@mail.ru>
2023-04-01 18:54:33 +0300

    Task 1 done

--- homeproject1.java
+++ homeproject1.java
@@ -1,5 +1,19 @@
-public class homeproject1 {
+import java.util.Scanner;
+
+/* Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n) */
+
+
+public class HomeProject1 {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner iScanner = new Scanner(System.in);
+        System.out.println("Enter number >");
+        int number = iScanner.nextInt();
+        int sum = 0;
+        int mult = 1;
+        for (int i = 1; i <= number; i++) {
+            sum += i;
+            mult *= i;
+        }
+        System.out.printf("sum of numbers: %d\n multiplication of numbers: %d\n", sum, mult);
         }
     }
