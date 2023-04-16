bef79a33b6a96fcc17bed2a70ad12f036b395c82
Nick Volodin <Volod-nick@mail.ru>
2023-04-16 16:13:02 +0300

    Task 2 done

---
+++ Task1Sem2/Task2.java
@@ -0,0 +1,66 @@
+package Task1Sem2;
+
+import java.io.InputStream;
+import java.util.Scanner;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+import java.util.logging.XMLFormatter;
+
+public class Task2 {
+    /* Реализуйте алгоритм сортировки пузырьком числового массива, результат после каждой итерации запишите в лог-файл. */
+
+    /* private static Logger log = Logger.getLogger(Task2.class.getName());
+  static {
+    try {
+      InputStream stream = Task2.class.getClassLoader()
+          .getResourceAsStream("log.xml");
+        LogManager.getLogManager().readConfiguration(stream);
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+ */
+
+    public static void bubbleSort(int[] sortArr) {
+        
+        for (int i = 0; i < sortArr.length - 1; i++) {
+            for(int j = 0; j < sortArr.length - i - 1; j++) {
+                if(sortArr[j + 1] < sortArr[j]) {
+                    int swap = sortArr[j];
+                    sortArr[j] = sortArr[j + 1];
+                    sortArr[j + 1] = swap;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        /* log.log(Level.INFO, "тестовое логирование"); */
+        Logger log = Logger.getLogger(Task2.class.getName());
+        ConsoleHandler ch = new ConsoleHandler();
+        /*  FileHandler fh = new FileHandler("log.xml");  */// получается только с указанием абсолютного пути
+        log.addHandler(ch);
+        SimpleFormatter sFormat = new SimpleFormatter();
+        /* XMLFormatter xml = new XMLFormatter(); */
+        ch.setFormatter(sFormat);
+        /* fh.setFormatter(xml); */
+        Scanner iScanner = new Scanner(System.in);
+        System.out.println("Enter lenght of array fo sort >");
+        int number = iScanner.nextInt();
+        iScanner.close();
+        int[] sortArr = new int[number];
+        for (int i = 0; i < sortArr.length; i++) {
+            sortArr[i] = (int)(Math.random() * 100);
+        }
+            bubbleSort(sortArr);
+            for(int i = 0; i < sortArr.length; i++){
+                System.out.print(sortArr[i] + " ");
+            }
+        log.info("тестовое логирование");
+    }
+
+}
